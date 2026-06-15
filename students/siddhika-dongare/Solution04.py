9e5753d39488af0f023fb7b9d0534dff0800f5de
siddhikadongare98-code <siddhikadongare98@gmail.com>
2026-06-15 11:11:58 +0530

    Update Solution04.py

--- students/siddhika-dongare/Solution04.py
+++ students/siddhika-dongare/Solution04.py
@@ -1,1 +1,34 @@
+#problem1
+def countdown(n):
+    while n >= 1:
+        print(n)
+        n = n - 1
+    print("Liftoff!")
 
+# Example
+countdown(5)
+
+#problem2
+def first_multiple(n, start):
+    x = start
+
+    while True:
+        if x % n == 0:
+            break
+        x = x + 1
+
+    return x
+
+# Example
+print(first_multiple(5, 12))
+
+
+#problem3
+def print_triangle(height):
+    row = 1
+    while row <= height:
+        print("*" * row)
+        row += 1
+
+# Example
+print_triangle(5)
