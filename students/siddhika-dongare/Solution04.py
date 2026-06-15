13eab981fcc78e4c5cde9f020eccc7fc2c889e85
siddhikadongare98-code <siddhikadongare98@gmail.com>
2026-06-15 11:19:57 +0530

    Update Solution04.py

--- students/siddhika-dongare/Solution04.py
+++ students/siddhika-dongare/Solution04.py
@@ -32,3 +32,14 @@ def print_triangle(height):
 
 # Example
 print_triangle(5)
+
+#problem4
+def is_prime(n):
+    if n <= 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
